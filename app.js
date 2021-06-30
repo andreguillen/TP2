27be5d2875dd06d8c52ca957a560b3ac22506974
Andre Guillen <andreguillen@gmail.com>
2021-06-30 16:09:33 -0300

    Cambios luego de pull

--- app.js
+++ app.js
@@ -3,13 +3,22 @@ const path = require('path');
 
 const app = express();
 
-app.get('/mundo', (req,res) =>{
-   res.sendFile(path.join(dirname, './views/index.html'));  // Permite enviar un archivo HTML
+app.use(express.static(path.resolve(__dirname, './public')));
+
+
+app.listen(3000, () => console.log ("Servidor corriendo en puerto 3000"));
+
+app.get('/', (req,res) =>{
+   res.sendFile(path.resolve(__dirname, './views/index.html'));  
+
 });
 
-app.use(express.static(path.join(dirname, './public')));
+
 //app.use(express.static(path.join(dirname, './views'))); en algun momento voy a necesitar poner publica esta carpeta 
 
-app.listen(3000, () => {
-    console.log("Servidor corriendo");
-});
+
+
+
+
+
+
