44f677aef34b76a9dfe7c5c8c815e139c2e5b6c7
Andre Guillen <andreguillen@gmail.com>
2021-07-11 22:47:54 -0300

    Trabajo terminado

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, './public')));
+app.listen(3000, () => { 
+   console.log ("Servidor corriendo en puerto 3000");
+});
 
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.listen(3000, () => console.log ("Servidor corriendo en puerto 3000"));
-
-app.get('/', (req,res) =>{
-   res.sendFile(path.resolve(__dirname, './views/index.html'));  
+app.get('/', (req,res) =>{   
+    //res.send("Hola Mundo");  // Permite enviar texto o codigo HTML
+   res.sendFile(path.resolve(__dirname, './views/index.html'));   // Permite enviar un archivo HTML
 
 });
+
+
+
+
 
 
 //app.use(express.static(path.join(dirname, './views'))); en algun momento voy a necesitar poner publica esta carpeta 
